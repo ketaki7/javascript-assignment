c3776e93f2def6823ab8d676e3057547c9fc81d9
ketaki7 <87003168+ketaki7@users.noreply.github.com>
2021-07-11 14:23:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function val()
+{
+
+    var a = document.querySelector('input');
+    var data = a.value;
+    innerText=data;
+    
+    document.querySelector('h1').innerText="Hi "+data;
+
+}
+
